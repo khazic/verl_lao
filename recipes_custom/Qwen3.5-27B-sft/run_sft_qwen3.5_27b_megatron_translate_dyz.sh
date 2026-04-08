d7208ecf0f300254d0e453d7a398f6751b86c933
khazic <khazzz1c@gmail.com>
2026-04-08 14:33:16 +0800

    recipes_custom: update dyz 27b project_name to 0408

--- recipes_custom/Qwen3.5-27B-sft/run_sft_qwen3.5_27b_megatron_translate_dyz.sh
+++ recipes_custom/Qwen3.5-27B-sft/run_sft_qwen3.5_27b_megatron_translate_dyz.sh
@@ -38,7 +38,7 @@ echo ">>> 数据文件: ${TRAIN_FILES}, total_epochs=${TOTAL_EPOCHS}"
 BACKEND=megatron
 RESUME_MODE=${RESUME_MODE:-disable}
 
-project_name=verl_sft_qwen3_5_27b_megatron_translate_0404_dyz
+project_name=verl_sft_qwen3_5_27b_megatron_translate_0408_dyz
 exp_name=qwen3_5_27b_megatron_translate_0325-${BACKEND}-tp${TP_SIZE}-pp${PP_SIZE}-cp${CP_SIZE}
 ckpts_home=${ckpts_home:-/llm-align/liuchonghan/ckpt_verl/sft/${project_name}/${exp_name}}
 
